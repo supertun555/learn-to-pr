3cdbd1de62bd71fc485fcf9a28430ac763e365dd
3 BeeBee <miw1513@hotmail.com>
2017-10-14 16:17:18 +0700

    add url

--- pages/index.vue
+++ pages/index.vue
@@ -6,11 +6,8 @@
       <nuxt-link to="Rachata">Rachata</nuxt-link>
       <nuxt-link to="/sirirat">sirirat</nuxt-link>
       <nuxt-link to="warakana">About</nuxt-link>
-<<<<<<< HEAD
       <nuxt-link to="Rittiphon">Rittiphon</nuxt-link>
-=======
       <nuxt-link to="/chonticha">chonticha</nuxt-link>
->>>>>>> 4b1b051c78b70089cac95fc55f196670a206b992
     </div>
   </section>
 </template>
