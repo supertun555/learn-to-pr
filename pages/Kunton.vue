5140e5a25e7969e10e65cef4fc1ceae46848c456
Kunton <supertun37299@hotmail.com>
2017-10-14 17:03:28 +0700

    add Kunton.vue

--- pages/Kunton.vue
+++ pages/Kunton.vue
@@ -1,11 +1,6 @@
-<head>
-  <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
-  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
-</head>
 <template>
   <section class="container">
+  <nuxt-link to="Kunton">Kunton</nuxt-link>
     <div>
       <h1>Learn to PR</h1>
       <nuxt-link to="Sahphan">Sahaphan</nuxt-link> <br>
